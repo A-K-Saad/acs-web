7c3d9d39f384ece910b55d1bf4e5282250bb5b11
A-K-Saad <aksaad.wd@gmail.com>
2024-12-10 19:31:09 +0600

    single page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { useEffect } from "react";
-import { Route, Routes, useLocation } from "react-router-dom";
-import Home from "./pages/Home";
+import { useLocation } from "react-router-dom";
 import "./db/sdk";
+import AboutMe from "./components/Home/AboutMe";
+import Contact from "./components/Home/Contact";
+import Navigation from "./components/Home/Navigation";
+import Projects from "./components/Home/Projects";
+import Skills from "./components/Home/Skills";
 
 const App = () => {
   const { hash, pathname } = useLocation();
@@ -24,9 +28,15 @@ const App = () => {
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-      </Routes>
+      <div className="container m-auto px-4 lg:px-12">
+        <Navigation />
+        <main className="py-5 md:py-10 space-y-24">
+          <AboutMe />
+          <Skills />
+          <Projects />
+          <Contact />
+        </main>
+      </div>
     </>
   );
 };
